7b360b01a41360c2698c0bb37eda657271692169
Dmitry <dmitry.balduev@gmail.com>
2021-03-20 23:49:16 -0400

    added feature to display current weather:

--- assets/script.js
+++ assets/script.js
@@ -19,8 +19,35 @@ function getCurrentWeather() {
         return response.json();
       })
       .then(function (data) {
-        console.log(data);
+        displayCurrentWeather(data);
+        displayUV(data);
       });
+
+     
   }
-  
+
+function displayCurrentWeather(info){
+    // let uvindex = getUV(info);
+    $("#city").text("City: " + info.name);
+    let tempKelvin = info.main.temp;
+    let tempF = Math.round((tempKelvin - 273.15)*9/5 + 32);
+    $("#temperature").text("Temperature: " + tempF + "F");
+    $("#humidity").text("Humidity: " + info.main.humidity + "%");
+    $("#wind-speed").text("Wind: " + info.wind.speed);
+    // $("#uv-index").text("UV-Index: " + uvindex);
+}
+function displayUV(info){
+    let url="http://api.openweathermap.org/data/2.5/uvi";
+    let lat = info.coord.lat;
+    let lon = info.coord.lon;
+    let uv;
+    fetch(url + "?lat=" + lat + "&lon=" + lon + "&appid=" + apikey)
+        .then(function (response){
+            return response.json;
+        })
+        .then(function(data){
+            console.log(data.value);
+            $("#uv-index").text("UV-Index: " + data.value);
+        })
+}
   $("button").on('click', getCurrentWeather);
